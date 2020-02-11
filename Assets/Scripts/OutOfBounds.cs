8048a75eacdef88664c06219ee3dff7a4920beb4
Nicholas Trotter <ntrotter@Nicholass-MacBook-Pro-7.local>
2020-02-11 13:41:25 -0600

    Adds respawning and deals with out of bounds (falling) Allows for death triggers to send back to respawn point Added additional prefab for structure and began a new scene to work on

---
+++ Assets/Scripts/OutOfBounds.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutOfBounds : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player"){
+            other.gameObject.GetComponent<Respawner>().playerRespawn();
+        }
+    }
+
+}
---
+++ Assets/Scripts/Respawner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawner : MonoBehaviour
+{
+    public Vector3 respawnPoint;
+
+    public void playerRespawn(){
+        //currently resets player to one spawn point
+        Transform playerTransform = GetComponentInParent<Transform>();
+        playerTransform.position = respawnPoint;
+    }
+}
