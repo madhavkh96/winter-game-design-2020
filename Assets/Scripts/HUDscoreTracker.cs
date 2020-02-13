1f0ac1baf593613095bfd69b3717c474be07b476
Nicholas Trotter <ntrotter@Nicholass-MacBook-Pro-7.local>
2020-02-13 14:15:10 -0600

    Adds coins for objectives: these add to score wwhich is displayed in upper right HUD Coins rotate and despawn when picked up with sfx

---
+++ Assets/Scripts/HUDscoreTracker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUDscoreTracker : MonoBehaviour
+{
+    private int score = 0;
+    private GUIStyle guiFontStyle = new GUIStyle();
+
+    public void addScore(int value)
+    {
+        score += value;
+    }
+
+    void Start()
+    {
+        guiFontStyle.fontSize = 48;
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(1200, 50, 200, 100), score.ToString(), guiFontStyle);
+    }
+}
